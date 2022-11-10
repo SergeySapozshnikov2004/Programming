6f69139061d9dd53d929919b6c72aa788c554fff
SergeySapozshnikov2004 <94596189+SergeySapozshnikov2004@users.noreply.github.com>
2022-11-10 17:23:43 +0300

    Update 2.2 вариант 7.cpp

--- 2.2 вариант 7.cpp
+++ 2.2 вариант 7.cpp
@@ -6,21 +6,21 @@
 using namespace std;
 
 /**
- * \brief Это функция выполняет вычисление по заданной формуле
- * \param x параметр х
- * \param y параметр y
+ * \brief Это функция выполняет вычисление при a > 1.5
+ * \param a параметр a
+ * \param x параметр x
  * \return значение функции
  */
-double getA1(const double a, const double x);
-double getA2(const double a, const double x);
+double getLager(const double a, const double x);
+
 
 /**
- * \brief Это функция выполняет вычисление по заданной формуле
- * \param x параметр х
- * \param y параметр y
- * \param z параметр z
+ * \brief Это функция выполняет вычисление при a <= 1.5
+ * \param a параметр a
+ * \param x параметр x
  * \return 0  значение функции
  */
+double getSmaller(const double a, const double x);
 
 /**
  * \brief Точка входа в программу
@@ -29,36 +29,33 @@ double getA2(const double a, const double x);
 int main()
 {
     double a = 1.5;
-    cout << std::setprecision(9) << "Введите X" << endl;
+    cout << "Введите X" << endl;
     double x;
     cin >> x;
 
     
-    const double a1 = getA1(a, x);
-    const double a2 = getA2(a, x);
+    const double a1 = getLager(a, x);
+    const double a2 = getSmaller(a, x);
    
     
     
     if ((a * x) < 1) { 
-        cout << setprecision(9) << "Y = " << a1 << endl;
+        cout << "Y = " << a1 << endl;
     }
     
     else { 
-        cout << setprecision(9) << "Y = " << a2 << endl;
+        cout << "Y = " << a2 << endl;
     }
     
 }
 
 
-double getA1(const double a, const double x)
+double getLager(const double a, const double x)
 {
     return a * x - log10(a * x);
 }
 
-double getA2(const double a, const double x)
+double getSmaller(const double a, const double x)
 {
     return a * x + log10(a * x);
 }
-
-
-
